2a9ccce1aef19efcc0591aaefee5383c63ab4f85
Kim Blomqvist <kblomqvist@iki.fi>
2012-06-09 14:04:03 +0300

    It was not intended to change the default src/main.c, though

--- src/main.c
+++ src/main.c
@@ -1,15 +1,13 @@
 #include <stdbool.h>
-#include "aery32/gpio.hh"
+#include "aery32/gpio.h"
 #include "board.h"
 
 #define LED AVR32_PIN_PC04
 
-using namespace aery;
-
 int main(void)
 {
 	init_board();
-	gpio_init_pin(
+	aery_gpio_init_pin(
 		LED,                  /* gpio pin number: 0-69 */
 		GPIO_OUTPUT|GPIO_HIGH /* options */
 	);
