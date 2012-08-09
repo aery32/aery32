72f54b1b49006093f8418245d73d768af42e6833
Kim Blomqvist <kblomqvist@iki.fi>
2012-08-09 19:48:47 +0300

    Update pwm example

--- examples/pwm.cpp
+++ examples/pwm.cpp
@@ -5,27 +5,30 @@ using namespace aery;
 
 int main(void)
 {
-	double D = 0.0;
+	uint8_t pwmcha = 2;
+	double dutycl = 0.0;
 
 	init_board();
 	gpio_init_pin(LED, GPIO_OUTPUT);
 	gpio_init_pin(AVR32_PIN_PB21, GPIO_FUNCTION_A);
 
-	pwm_init_channel(2, MCK_DIVIDED_BY_64);
-	pwm_setup_chamode(2, LEFT_ALIGNED, START_LOW);
-	pwm_enable(1 << 2);
+	/* Init and enable PWM channel two */
+	pwm_init_channel(pwmcha, MCK_DIVIDED_BY_128);
+	pwm_setup_chamode(pwmcha, LEFT_ALIGNED, START_LOW);
+	pwm_enable(1 << pwmcha);
 
 	/* All done. Turn the LED on. */
 	gpio_set_pin_high(LED);
 
 	for(;;) {
-		for (; D <= 1.0; D += 0.1) {
-			pwm_update_dutycl(2, D);
-			pwm_wait_periods(2, 1);
+		/* Connect LED to pin PB21 and it blinks smoothly */
+		for (; dutycl <= 1.0; dutycl += 0.05) {
+			pwm_update_dutycl(pwmcha, dutycl);
+			pwm_wait_periods(pwmcha, 2);
 		}
-		for (; D >= 0.0; D -= 0.1) {
-			pwm_update_dutycl(2, D);
-			pwm_wait_periods(2, 1);
+		for (; dutycl >= 0.0; dutycl -= 0.05) {
+			pwm_update_dutycl(pwmcha, dutycl);
+			pwm_wait_periods(pwmcha, 2);
 		}
 	}
 
