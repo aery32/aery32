5ebdd6506a22a9ed87715f864cf1f3ebf8432ac6
Kim Blomqvist <kblomqvist@iki.fi>
2012-09-16 12:36:19 +0300

    Fix adc examples. Add hd44780+adc example to CI.

--- examples/adc.cpp
+++ examples/adc.cpp
@@ -7,7 +7,6 @@ using namespace aery;
 
 int main(void)
 {
-	int errno;
 	uint16_t result; /* conversion result in bits */
 	double volt;     /* conversion in volts */
 
@@ -18,22 +17,23 @@ int main(void)
 	init_board();
 	gpio_init_pins(porta, ADC_PINMASK_ALLCHAN, GPIO_FUNCTION_A);
 
-	errno = adc_init(
+	adc_init(
 		7,    /* prescal, adclk = pba_clk / (2 * (prescal+1)) */
 		true, /* hires, 10-bit (false would be 8-bit) */
 		0,    /* shtim, sample and hold time = (shtim + 3) / adclk */
 		0     /* startup, startup time = (startup + 1) * 8 / adclk */
 	);
 
-	if (errno != -1)
-		adc_enable(1 << 3); /* enables the channel 3 */
+	/* Enable ADC channel 3, PA24 */
+	adc_enable((1 << 3) /* 8-bit channel mask */);
+
 
 	/* All done. Turn the LED on. */
 	gpio_set_pin_high(LED);
 
 	for(;;) {
 		adc_start_cnv();
-		while (adc_isbusy(1 << 3));
+		while (adc_isbusy(1 << 3) /* 8-bit channel mask */);
 		result = adc_read_cnv(3);
 		volt = cnv2volt(result);
 	}
