af70ec3d06a413b7074a4d7fea0420e48f7f0858
Kim Blomqvist <kblomqvist@iki.fi>
2013-01-13 15:33:10 +0200

    Add teaser USB Hello

---
+++ examples/usb_hello.cpp
@@ -0,0 +1,27 @@
+#include <aery32/all.h>
+#include "board.h"
+
+using namespace aery;
+
+#define LED AVR32_PIN_PC04
+
+int main(void)
+{
+	/*
+	 * Put your application initialization sequence here. The default
+	 * board initializer defines all pins as input and sets the CPU clock
+	 * speed to 66 MHz.
+	 */
+	board::init();
+
+	gpio_init_pin(LED, GPIO_OUTPUT);
+	gpio_set_pin_high(LED);
+
+	for(;;) {
+		/* Put your application code here */
+
+		board::usb << "hello\r\n";
+	}
+
+	return 0;
+}
