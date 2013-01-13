7091264e725434b30dcae580cf536a6c6d48b02a
Kim Blomqvist <kblomqvist@iki.fi>
2013-01-13 16:50:24 +0200

    Update gpio_extint_pin_change and rtc_interrupt examples

--- examples/rtc_interrupt.cpp
+++ examples/rtc_interrupt.cpp
@@ -1,5 +1,9 @@
+#include <aery32/all.h>
 #include "board.h"
+
 using namespace aery;
+
+#define LED AVR32_PIN_PC04
 
 void isrhandler_rtc(void)
 {
@@ -9,6 +13,11 @@ void isrhandler_rtc(void)
 
 int main(void)
 {
+	/*
+	 * Put your application initialization sequence here. The default
+	 * board initializer defines all pins as input and sets the CPU clock
+	 * speed to 66 MHz.
+	 */
 	board::init();
 	gpio_init_pin(LED, GPIO_OUTPUT|GPIO_HIGH);
 
