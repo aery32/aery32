52cb10777438fd2ec8f26e75755b4565f5010a82
Kim Blomqvist <kblomqvist@iki.fi>
2013-01-13 15:10:38 +0200

    Revert default main.cpp and board.cpp/h to be more specific with includes and LED pin definition

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
+#include <aery32/all.h>
 #include "board.h"
+
 using namespace aery;
+
+#define LED AVR32_PIN_PC04
 
 int main(void)
 {
 	/*
 	 * Put your application initialization sequence here. The default
-	 * board initializer defines the LED pin as output and sets the CPU
-	 * clock speed to 66 MHz.
+	 * board initializer defines all pins as input and sets the CPU clock
+	 * speed to 66 MHz.
 	 */
 	board::init();
+
+	gpio_init_pin(LED, GPIO_OUTPUT);
 	gpio_set_pin_high(LED);
 
 	for(;;) {
