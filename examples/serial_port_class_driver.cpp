a2c888a655b3ba2b34b01de115eaf28d7e120604
Kim Blomqvist <kblomqvist@iki.fi>
2013-03-16 14:03:23 +0200

    Formal changes to examples/serial_port_class_driver.cpp

--- examples/serial_port_class_driver.cpp
+++ examples/serial_port_class_driver.cpp
@@ -2,7 +2,7 @@
 #include "board.h"
 
 #define LED			AVR32_PIN_PC04
-#define UART0_SERIAL_PINMASK	0x3		// PA0 = RX, PA01 = TX
+#define UART0_SERIAL_PINMASK	0x3 // PA0 = RX, PA01 = TX
 
 volatile uint8_t bufdma0[128] = {};
 volatile uint8_t bufdma1[128] = {};
@@ -18,32 +18,24 @@ int main(void)
 	double d = 0.0;
 
 	/*
-	 * Put your application initialization sequence here. The default
-	 * board initializer defines all pins as input and sets the CPU clock
-	 * speed to 66 MHz.
+	 * The default board initializer defines all pins as input and
+	 * sets the CPU clock speed to 66 MHz.
 	 */
 	board::init();
 	gpio_init_pin(LED, GPIO_OUTPUT);
 	gpio_init_pins(porta, UART0_SERIAL_PINMASK, GPIO_FUNCTION_A); 
 
 	/*
-	 * Instantiate input and output DMAs. You can consider these as
-	 * input/output buffers.
+	 * Instantiate input and output Peripheral DMA class drivers.
+	 * You can consider these as input and output buffers.
 	 */
-	periph_idma dma0 = periph_idma(0, AVR32_PDCA_PID_USART0_RX, bufdma0, sizeof(bufdma0));
-	periph_odma dma1 = periph_odma(1, AVR32_PDCA_PID_USART0_TX, bufdma1, sizeof(bufdma1));
+	periph_idma dma0 = periph_idma(0, AVR32_PDCA_PID_USART0_RX, bufdma0,
+		sizeof(bufdma0));
+	periph_odma dma1 = periph_odma(1, AVR32_PDCA_PID_USART0_TX, bufdma1,
+		sizeof(bufdma1));
 
-	/*
-	 * Instantiate serial port class driver with DMAs. Default speed
-	 * is 115200, parity none, 8 data bits and one stop bit.
-	 */
+	/* Default speed is 115200, parity none, 8 data bits, one stop bit */
 	serial_port pc = serial_port(usart0, dma0, dma1);
-
-	/*
-	 * Change the default line delimitation (or line ending recognition)
-	 * from the default "\r\n" to '\n'.
-	 */
-	pc.set_default_delim('\n');
 	pc.enable();
 
 	gpio_set_pin_high(LED);
@@ -54,7 +46,9 @@ int main(void)
 		/* Put your application code here */
 
 		pc.getline(line, &nread);
-		if (nread == 0) continue; /* skip blank line */
+		if (nread == 0)
+			continue; /* skip blank line */
+
 		pc << "'" << line << "' is " << nread << " characters long.\n";
 
 		sscanf(line, "%d %lf", &i, &d);
